8a77ecdceb213850cc632507e013df073e588968
Michael J Harvey <michael4d45@gmail.com>
2019-11-30 21:06:55 -0700

    refactored a bunch of the code

--- led.py
+++ led.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov 29 15:19:36 2019
 
-@author: armen
+@author: armen, harvey
 """
 
 import os
@@ -38,121 +39,102 @@ RED.start(0)
 BLUE.start(0)
 GREEN.start(0)
 
-
-def getTemp():
-    response = requests.request('GET', url, data=payload, headers=headers, params=querystring)
-    res = json.loads(response.text)
-    temperature = round((res['main']['temp'] - 273.15) * 1.8 + 32, 1)
-    return temperature
-
-def getPrecip():
-    response = requests.request('GET', url, data=payload, headers=headers, params=querystring)
-    res = json.loads(response.text)
-    if res['weather'][0]['id'] or res['weather'][-1]['id'] < 700:
-        return 1
-    else:
-        return 0
-
-Clear = lambda: os.system('clear')
-
+blink(RED, 0)
+blink(BLUE, 0)
+blink(GREEN, 0)
 
 temp = getTemp()
 precip = getPrecip()
 temps = [temp]
 times = [0]
-wait = 600 #s
+wait = 600 #10 min in seconds
+max_plot_len = int(24*3600/wait)
+
+start()
+end()
+
+clear = lambda: os.system('clear')
 
 
-def Append(temps,times):
-    temp = getTemp()
-    temps.insert(0,temp)
-    times.append(times[-1]-wait/3600)
-    if len(times)>24*3600/wait:
-        times = times[0:int(24*3600/wait)]
-        temps = temps[0:int(24*3600/wait)]
+def blink(COLOR, t):
+    for dc in range (0,101,5):
+        COLOR.ChangeDutyCycle(dc)
+        time.sleep(t)
+    for dc in range(100,-1,-5):
+        RED.ChangeDutyCycle(dc)
+        time.sleep(t)
 
-def flashTemp(T):
-    if T>=90:
-        for dc in range (0,101,5):
-            RED.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range(100,-1,-5):
-            RED.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range (0,101,5):
-            RED.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range(100,-1,-5):
-            RED.ChangeDutyCycle(dc)
-            time.sleep(.01)
-    elif T<90 and T>32:
-        for dc in range (0,101,5):
-            RED.ChangeDutyCycle(dc)
-            time.sleep(.1)
-        for dc in range(100,-1,-5):
-            RED.ChangeDutyCycle(dc)
-            time.sleep(.1)
-    elif T<=32 and T>0:
-        for dc in range (0,101,5):
-            BLUE.ChangeDutyCycle(dc)
-            time.sleep(.1)
-        for dc in range(100,-1,-5):
-            BLUE.ChangeDutyCycle(dc)
-            time.sleep(.1)
+
+def quickBlink(COLOR):
+    blink(COLOR, .01)
+    blink(COLOR, .01)
+
+
+def slowBlink(COLOR):
+    blink(COLOR, .1)
+
+
+def flashTemp():
+    if temp >= 90:
+        quickBlink(RED)
+    elif temp < 90 and temp > 32:
+        slowBlink(RED)
+    elif temp <= 32 and temp > 0:
+        slowBlink(BLUE)
     else:
-        for dc in range (0,101,5):
-            BLUE.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range(100,-1,-5):
-            BLUE.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range (0,101,5):
-            BLUE.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range(100,-1,-5):
-            BLUE.ChangeDutyCycle(dc)
-            time.sleep(.01)
-                
-def flashPrecip(binary):
-    if binary == 1:
-        for dc in range (0,101,5):
-            GREEN.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range(100,-1,-5):
-            GREEN.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range (0,101,5):
-            GREEN.ChangeDutyCycle(dc)
-            time.sleep(.01)
-        for dc in range(100,-1,-5):
-            GREEN.ChangeDutyCycle(dc)
-            time.sleep(.01)
+        quickBlink(BLUE)
+
+
+def flashPrecip():
+    if precip == 1:
+        quickBlink(GREEN)
     else:
         GREEN.ChangeDutyCycle(0)
-                
-            
-def Plot():
+
+
+def getTemp():
+    #request weather
+    response = requests.request('GET', url, data=payload, headers=headers, params=querystring)
+    res = json.loads(response.text)
+    #convert Kelvin to Fahrenheit
+    temp = round((res['main']['temp'] - 273.15) * 1.8 + 32, 1)
+    if res['weather'][0]['id'] or res['weather'][-1]['id'] < 700:
+        precip = 1
+    else:
+        precip = 0
+
+
+def append():
+    temps.insert(0,temp)
+    times.append(times[-1]-wait/3600)
+    if len(times)>max_plot_len:
+        times = times[0:max_plot_len]
+        temps = temps[0:max_plot_len]
+
+
+def plot():
+    clear()
     fig = tpl.figure()
-    fig.plot(times,temps)
+    fig.plot(times, temps)
     fig.show()
-        
-    
-start = time.time()
 
 
-while True:
-    now = time.time()
-    if now - start >= wait:
-        start = time.time()
-        Append(temps,times)
-        precip = getPrecip()
-        Clear()
-        Plot()
-    flashTemp(temp)
-    flashPrecip(precip)
-    time.sleep(1)
+def start():
+    start = time.time()
+    while True:
+        now = time.time()
+        if now - start >= wait:
+            start = time.time()
+            getWeather()
+            append()
+            plot()
+        flashTemp()
+        flashPrecip()
+        time.sleep(1)
 
 
-RED.stop()
-BLUE.stop()
-GPIO.cleanup()
+def end():
+    RED.stop()
+    BLUE.stop()
+    GPIO.cleanup()
+
